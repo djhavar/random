fd01283b76a1dad9ba642d025423370bac259331
djhavar <90192071+djhavar@users.noreply.github.com>
2022-05-02 16:51:07 +0530

    Add files via upload

---
+++ random0.ty.py
@@ -0,0 +1,17 @@
+colors=['red','white','brown']
+print (colors)
+
+name_of_the_students_in_the_class=['devik','shubh','anay','laksh','shivam']
+print(name_of_the_students_in_the_class)
+
+import random
+n = random.random()
+print(n)
+
+import random
+n = random.randint(0,10)
+print(n)
+
+import random
+randomlist = random.sample (range(80,100),3)
+print(randomlist)
